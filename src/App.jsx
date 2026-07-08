0575f1e8e5f683fa120f63e6045d05e4e47884cd
Swathi30-web <113751371+Swathi30-web@users.noreply.github.com>
2026-07-08 12:08:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import BentoProduct from './pages/BentoProduct';
-import Contact from './pages/Contact';
-import Work from './pages/Work';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BentoProduct from "./pages/BentoProduct";
+import Contact from "./pages/Contact";
+import Work from "./pages/Work";
+
 export default function App() {
   return (
-    <BrowserRouter basename="/Folio-app">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-       <Route path="/Work" element={<Work />} />
+        <Route path="/Work" element={<Work />} />
         <Route path="/bento" element={<BentoProduct />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
