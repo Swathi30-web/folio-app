7310ca27acf1c124820bbb4ecd8ade54e60352ec
Swathi30-web <113751371+Swathi30-web@users.noreply.github.com>
2026-07-08 12:42:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import BentoProduct from "./pages/BentoProduct";
-import Contact from "./pages/Contact";
-import Work from "./pages/Work";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Work from './pages/Work';
+import BentoProduct from './pages/BentoProduct';
+import Contact from './pages/Contact';
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Work />} />
+        <Route path="/Work" element={<Work />} />
         <Route path="/bento" element={<BentoProduct />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
