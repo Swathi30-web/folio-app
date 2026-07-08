244375625d7a423475829e142e4c2e5395fd3102
Swathi30-web <113751371+Swathi30-web@users.noreply.github.com>
2026-07-08 14:26:47 +0530

    Update OverviewSection.jsx

--- src/components/OverviewSection.jsx
+++ src/components/OverviewSection.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
 // Use these imports. If they turn grey/unused, the path is wrong!
-import bentoImage from "../assets/img.png";
-import icon1 from "../assets/img1.png";
-import icon2 from "../assets/img2.png";
-
+i
 const cardData = [
   {
     id: 1,
@@ -28,11 +25,7 @@ export default function OverviewSection() {
         {/* LEFT COLUMN */}
         <div className="lg:col-span-5 flex flex-col items-center">
           <div className="relative w-72 h-72 sm:w-85 sm:h-85 md:w-96 md:h-96 bg-purple-300 rounded-full flex justify-center items-center shadow-lg overflow-visible">
-            <img 
-              src={bentoImage} // Using the variable
-              alt="3D Visual Design System Layout" 
-              className="w-[110%] h-auto object-contain transform -translate-x-2 drop-shadow-xl"
-            />
+         
           </div>
         </div>
 
@@ -42,11 +35,7 @@ export default function OverviewSection() {
             {cardData.map((card) => (
               <div key={card.id} className="bg-white border border-slate-100 rounded-[32px] p-8 flex flex-col items-center">
                 <div className={`w-24 h-24 rounded-full flex justify-center items-center ${card.iconColor} bg-opacity-70`}>
-                  <img 
-                    src={card.iconImage} // Using the variable
-                    alt={card.text} 
-                    className="w-14 h-14 object-contain" 
-                  />
+                  
                 </div>
                 <p className="text-indigo-600 text-base md:text-lg font-bold">{card.text}</p>
               </div>
